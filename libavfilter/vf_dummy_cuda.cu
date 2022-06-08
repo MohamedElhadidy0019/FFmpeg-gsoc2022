60f2d50b4dfcfb68c0f257a299dc5662117f3138
Mohamed Khaled Mohamed <56936494+MohamedElhadidy0019@users.noreply.github.com>
2022-06-08 13:40:23 +0200

    encapsulated the core logic in a function

--- libavfilter/vf_dummy_cuda.cu
+++ libavfilter/vf_dummy_cuda.cu
@@ -22,290 +22,273 @@
 
 #include "cuda/vector_helpers.cuh"
 
-extern "C" {
-
-
-
-
-
-__device__ float minnn(float a, float b) {
-  return a < b ? a : b;
-}
-__device__ float maxxx(float a, float b) {
-  return a > b ? a : b;
-}
-__device__ float root(float n){
-  // Max and minnn are used to take into account numbers less than 1
-  float lo = minnn(1.0, n), hi = maxxx(1.0, n), mid;
-
-  // Update the bounds to be off the target by a factor of 10
-  while(100 * lo * lo < n) lo *= 10;
-  while(0.01 * hi * hi > n) hi *= 0.1;
-
-  for(int i = 0 ; i < 100 ; i++){
-      mid = (lo+hi)/2;
-      if(mid*mid == n) return mid;
-      if(mid*mid > n) hi = mid;
-      else lo = mid;
-  }
-  return mid;
-}
-
-
-
-
-/*
-function description : function convolves the edge detector 
-                        laplacian operator with the image
-
-input :
-    src_tex_Y: Y channel of source image normallized to [0,1]
-    src_tex_U: U channel of source image normallized to [0,1]
-    src_tex_V: V channel of source image normallized to [0,1]
-    dst_Y: Y channel of output convolved image [0,255]
-    dst_U: U channel of output convolved image [0,255] ,always zero as the Y channel only matters in edge detection
-    dst_V: V channel of output convolved image [0,255] ,always zero as the Y channel only matters in edge detection
-    width: width of sourceY image
-    height: height of sourceY image
-    pitch: pitch of sourceY image
-    width_uv: width of sourceU,V image
-    height_uv: height of sourceU,V image
-    pitch_uv: pitch of sourceU,V image
-*/
-
-__global__ void Process_uchar(cudaTextureObject_t src_tex_Y, cudaTextureObject_t src_tex_U, cudaTextureObject_t src_tex_V,
-                              uchar *dst_Y, uchar *dst_U, uchar *dst_V,
-                              int width, int height, int pitch,
-                              int width_uv, int height_uv, int pitch_uv)
+extern "C"
 {
-   
-    int window_size = 3; //size of window
-    int x = blockIdx.x * blockDim.x + threadIdx.x; // x coordinate of current pixel
-    int y = blockIdx.y * blockDim.y + threadIdx.y; // y coordinate of current pixel
-   
-    if (y >= height || x >= width)
-        return;
-    if (y >= height_uv || x >= width_uv)
-        return;
 
-
-    int y_index = y * pitch + x; // index of current pixel in sourceY , access the 1d array as a 2d one
-
-    int start_r = x - window_size / 2;
-    int start_c = y - window_size / 2;
-
-   
-    //green color
-    float u_chroma=48.0f;
-    float v_chroma=45.0f;
-    float similarity = 0.22f;
-
-    int counter=0;
-    float diff=0.0f;
-    float du,dv;
-    for (int i = 0; i < window_size; i++)
+    __device__ float minnn(float a, float b)
     {
-        for (int j = 0; j < window_size; j++)
+        return a < b ? a : b;
+    }
+    __device__ float maxxx(float a, float b)
+    {
+        return a > b ? a : b;
+    }
+    __device__ float root(float n)
+    {
+        // Max and minnn are used to take into account numbers less than 1
+        float lo = minnn(1.0, n), hi = maxxx(1.0, n), mid;
+
+        // Update the bounds to be off the target by a factor of 10
+        while (100 * lo * lo < n)
+            lo *= 10;
+        while (0.01 * hi * hi > n)
+            hi *= 0.1;
+
+        for (int i = 0; i < 100; i++)
         {
-            int r = start_r + i;
-            int c = start_c + j;
-            bool flag = (r >= 0 && r < width && c >= 0 && c < height);
-            if (flag)
+            mid = (lo + hi) / 2;
+            if (mid * mid == n)
+                return mid;
+            if (mid * mid > n)
+                hi = mid;
+            else
+                lo = mid;
+        }
+        return mid;
+    }
+
+    __device__ static inline bool is_transparent(cudaTextureObject_t src_tex,
+                            cudaTextureObject_t src_tex_V,
+                            int width_uv, int height_uv,
+                            int x, int y, float2 chromakey_uv,
+                            float similarity, bool is_uchar2)
+    {
+
+        int window_size = 3;
+        int start_r = x - window_size / 2;
+        int start_c = y - window_size / 2;
+
+        int counter = 0;
+        float diff = 0.0f;
+        float du, dv;
+
+        for (int i = 0; i < window_size; i++)
+        {
+            for (int j = 0; j < window_size; j++)
             {
-                du=(tex2D<float>(src_tex_U, r, c)*255.0f) - u_chroma;
-                dv=(tex2D<float>(src_tex_V, r, c)*255.0f) - v_chroma;
-                diff += root((du * du + dv * dv) / (255.0f * 255.0f * 2.f) );
+                int r = start_r + i;
+                int c = start_c + j;
+                bool flag = (r >= 0 && r < width_uv && c >= 0 && c < height_uv);
+
+                if (!flag)
+                    continue;
+
+                float u_value, v_value;
+                if (is_uchar2)
+                {
+                    float2 temp_uv = tex2D<float2>(src_tex, r, c);
+                    u_value = temp_uv.x;
+                    v_value = temp_uv.y;
+                }
+                else
+                {
+                    u_value = tex2D<float>(src_tex, r, c);
+                    v_value = tex2D<float>(src_tex_V, r, c);
+                }
+
+                du = (u_value * 255.0f) - chromakey_uv.x;
+                dv = (v_value * 255.0f) - chromakey_uv.y;
+                diff += root((du * du + dv * dv) / (255.0f * 255.0f * 2.f));
                 counter++;
             }
         }
-    }
-    if(counter>0)
-    {
-        diff=diff/counter;
-    }
-    else
-    {
-        diff/=9.0f;
-    }
-    
-    
-    int u_index, v_index;
-    v_index = u_index = y * pitch_uv + x;
-    int new_size=2;
 
-   
-    if(diff < similarity) //it is chroma
-    {
-        //white
-    //dst_Y[y_index] = 255;
-
-    for(int k=0;k<new_size;k++)
+        if (counter > 0)
         {
-            for(int l=0;l<new_size;l++)
-            {
-                int x_resize=x*new_size+k;
-                int y_resize=y*new_size+l;
-                int y_channel_resize=y_resize*pitch+x_resize;
-                if (y_resize >= height || x_resize >= width)
-                    continue;;
-
-                dst_Y[y_channel_resize] = 255; 
-
-            }
-        }   
-
-    dst_U[u_index] = 128;
-    dst_V[v_index] = 128;
-    
-    }
-    else{ //not chroma
-        //black
-        //dst_Y[y_index] = 0;
-        for(int k=0;k<new_size;k++)
+            diff = diff / counter;
+        }
+        else
         {
-            for(int l=0;l<new_size;l++)
-            {
-                int x_resize=x*new_size+k;
-                int y_resize=y*new_size+l;
-                int y_channel_resize=y_resize*pitch+x_resize;
-                if (y_resize >= height || x_resize >= width)
-                    continue;;
+            diff /= 9.0f;
+        }
 
-                dst_Y[y_channel_resize] = 0; 
-
-            }
-        }   
-        dst_U[u_index] = 128;
-        dst_V[v_index] = 128;
+        return diff < similarity ? true : false;
     }
-    
 
+    /*
+    function description : function that genrates the alpha channel for a chroma keyed picture
 
-    
-}
+    input :
+        src_tex_Y: Y channel of source image normallized to [0,1]
+        src_tex_U: U channel of source image normallized to [0,1]
+        src_tex_V: V channel of source image normallized to [0,1]
+        dst_Y: Y channel of output convolved image [0,255]
+        dst_U: U channel of output convolved image [0,255] ,always zero as the Y channel only matters in edge detection
+        dst_V: V channel of output convolved image [0,255] ,always zero as the Y channel only matters in edge detection
+        width: width of sourceY image
+        height: height of sourceY image
+        pitch: pitch of sourceY image
+        width_uv: width of sourceU,V image
+        height_uv: height of sourceU,V image
+        pitch_uv: pitch of sourceU,V image
+    */
 
-
-
-
-
-
-//function to prtotoype chroma keing
-
-__global__ void Process_uchar2(cudaTextureObject_t src_tex_Y, cudaTextureObject_t src_tex_UV, cudaTextureObject_t unused1,
-                               uchar *dst_Y, uchar2 *dst_UV, uchar *unused2,
-                               int width, int height, int pitch,
-                               int width_uv, int height_uv, int pitch_uv)
-{
-
-  
-    int window_size = 3; //size of window 
-    int x = blockIdx.x * blockDim.x + threadIdx.x; // x coordinate of current pixel
-    int y = blockIdx.y * blockDim.y + threadIdx.y; // y coordinate of current pixel
-    
-    if (y >= height || x >= width)
-        return;
-    if (y >= height_uv || x >= width_uv)
-        return;
-
-
-    int y_index = y * pitch + x; // index of current pixel in sourceY , access the 1d array as a 2d one
-
-    int start_r = x - window_size / 2;
-    int start_c = y - window_size / 2;
-    int temp = 0; 
-
-    
-    //green color
-    float u_chroma=48.0f;
-    float v_chroma=45.0f;
-    float similarity = 0.22f;
-
-
-  
-    int counter=0;
-    float diff=0.0f;
-    float du,dv;
-
-    //this loop covers the eight neghbour of the pixel
-    for (int i = 0; i < window_size; i++)
+    __global__ void Process_uchar(cudaTextureObject_t src_tex_Y, cudaTextureObject_t src_tex_U, cudaTextureObject_t src_tex_V,
+                                  uchar *dst_Y, uchar *dst_U, uchar *dst_V,
+                                  int width, int height, int pitch,
+                                  int width_uv, int height_uv, int pitch_uv)
     {
-        for (int j = 0; j < window_size; j++)
-        {
-            int r = start_r + i;
-            int c = start_c + j;
-            bool flag = r >= 0 && r < width && c >= 0 && c < height;
-            if (flag)
-            {
-                float2 uv=tex2D<float2>(src_tex_UV, r, c)*255.0f;
 
-                du=uv.x - u_chroma;
-                dv=uv.y - v_chroma;
-                diff += root(  (du * du + dv * dv) / (255.0f * 255.0f * 2.0f)    );
-                counter++;
+        int window_size = 3;                           // size of window
+        int x = blockIdx.x * blockDim.x + threadIdx.x; // x coordinate of current pixel
+        int y = blockIdx.y * blockDim.y + threadIdx.y; // y coordinate of current pixel
+
+        if (y >= height || x >= width)
+            return;
+        if (y >= height_uv || x >= width_uv)
+            return;
+
+        int y_index = y * pitch + x; // index of current pixel in sourceY , access the 1d array as a 2d one
+
+        int start_r = x - window_size / 2;
+        int start_c = y - window_size / 2;
+
+        // green color
+        float u_chroma = 48.0f;
+        float v_chroma = 45.0f;
+        float similarity = 0.20f;
+        bool flag_alpha=is_transparent(src_tex_U,src_tex_V,width_uv,height_uv,x,y,make_float2(u_chroma,v_chroma),similarity,true);
+
+
+        int u_index, v_index;
+        v_index = u_index = y * pitch_uv + x;
+        int new_size = 2;
+
+        if (flag_alpha) // it is chroma
+        {
+            // white
+            // dst_Y[y_index] = 255;
+
+            for (int k = 0; k < new_size; k++)
+            {
+                for (int l = 0; l < new_size; l++)
+                {
+                    int x_resize = x * new_size + k;
+                    int y_resize = y * new_size + l;
+                    int y_channel_resize = y_resize * pitch + x_resize;
+                    if (y_resize >= height || x_resize >= width)
+                        continue;
+                    ;
+
+                    dst_Y[y_channel_resize] = 255;
+                }
             }
+
+            dst_U[u_index] = 128;
+            dst_V[v_index] = 128;
+        }
+        else
+        { // not chroma
+            // black
+            // dst_Y[y_index] = 0;
+            for (int k = 0; k < new_size; k++)
+            {
+                for (int l = 0; l < new_size; l++)
+                {
+                    int x_resize = x * new_size + k;
+                    int y_resize = y * new_size + l;
+                    int y_channel_resize = y_resize * pitch + x_resize;
+                    if (y_resize >= height || x_resize >= width)
+                        continue;
+                    ;
+
+                    dst_Y[y_channel_resize] = 0;
+                }
+            }
+            dst_U[u_index] = 128;
+            dst_V[v_index] = 128;
         }
     }
-    if(counter>0)
-    {
-        diff=diff/counter;
-    }
-    else
-    {
-        diff/=9.0f;
-    }
-    
-    
-    int u_index, uv_index;
-    uv_index = u_index = y * pitch_uv + x;
 
-    
+    // function to prtotoype chroma keing
 
-   int new_size=2;
-    if(diff < similarity)   //it is chroma 
+    __global__ void Process_uchar2(cudaTextureObject_t src_tex_Y, cudaTextureObject_t src_tex_UV, cudaTextureObject_t unused1,
+                                   uchar *dst_Y, uchar2 *dst_UV, uchar *unused2,
+                                   int width, int height, int pitch,
+                                   int width_uv, int height_uv, int pitch_uv)
     {
-    
-        //white
-        //dst_Y[y_index] = 255; 
-        for(int k=0;k<new_size;k++)
+
+        int window_size = 3;                           // size of window
+        int x = blockIdx.x * blockDim.x + threadIdx.x; // x coordinate of current pixel
+        int y = blockIdx.y * blockDim.y + threadIdx.y; // y coordinate of current pixel
+
+        if (y >= height || x >= width)
+            return;
+        if (y >= height_uv || x >= width_uv)
+            return;
+
+        int y_index = y * pitch + x; // index of current pixel in sourceY , access the 1d array as a 2d one
+
+        int start_r = x - window_size / 2;
+        int start_c = y - window_size / 2;
+        int temp = 0;
+
+        // green color
+        float u_chroma = 48.0f;
+        float v_chroma = 45.0f;
+        float similarity = 0.22f;
+
+        int counter = 0;
+        float diff = 0.0f;
+        float du, dv;
+        bool flag_alpha=is_transparent(src_tex_UV,unused1,width_uv,height_uv,x,y,make_float2(u_chroma,v_chroma),similarity,true);
+        
+        
+
+        int u_index, uv_index;
+        uv_index = u_index = y * pitch_uv + x;
+
+        int new_size = 2;
+        if (flag_alpha) // it is chroma
         {
-            for(int l=0;l<new_size;l++)
+
+            // white
+            // dst_Y[y_index] = 255;
+            for (int k = 0; k < new_size; k++)
             {
-                int x_resize=x*new_size+k;
-                int y_resize=y*new_size+l;
-                int y_channel_resize=y_resize*pitch+x_resize;
-                if (y_resize >= height || x_resize >= width)
-                    continue;;
+                for (int l = 0; l < new_size; l++)
+                {
+                    int x_resize = x * new_size + k;
+                    int y_resize = y * new_size + l;
+                    int y_channel_resize = y_resize * pitch + x_resize;
+                    if (y_resize >= height || x_resize >= width)
+                        continue;
+                    ;
 
-                dst_Y[y_channel_resize] = 255; 
-
+                    dst_Y[y_channel_resize] = 255;
+                }
             }
-        }   
 
-        dst_UV[uv_index] = make_uchar2(128,128);
-    
-    }
-    else    // it is not chroma
-    {
-        //black
-        //dst_Y[y_index] = 0; 
-        for(int k=0;k<new_size;k++)
+            dst_UV[uv_index] = make_uchar2(128, 128);
+        }
+        else // it is not chroma
         {
-            for(int l=0;l<new_size;l++)
+            // black
+            // dst_Y[y_index] = 0;
+            for (int k = 0; k < new_size; k++)
             {
-                int x_resize=x*new_size+k;
-                int y_resize=y*new_size+l;
-                if (y_resize >= height || x_resize >= width)
-                    continue;;
-                int y_channel_resize=y_resize*pitch+x_resize;
-                dst_Y[y_channel_resize] = 0; 
-
-
+                for (int l = 0; l < new_size; l++)
+                {
+                    int x_resize = x * new_size + k;
+                    int y_resize = y * new_size + l;
+                    if (y_resize >= height || x_resize >= width)
+                        continue;
+                    ;
+                    int y_channel_resize = y_resize * pitch + x_resize;
+                    dst_Y[y_channel_resize] = 0;
+                }
             }
-        }   
-        dst_UV[uv_index] = make_uchar2(128,128);
-
+            dst_UV[uv_index] = make_uchar2(128, 128);
+        }
     }
-
-}
-
 }
