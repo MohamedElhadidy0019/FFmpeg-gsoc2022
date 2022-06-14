bc0953ea31c6d1ee1f79c592b82903984547ded4
Mohamed Khaled Mohamed <56936494+MohamedElhadidy0019@users.noreply.github.com>
2022-06-14 11:26:54 +0200

    Revert "working but weird"
    This reverts commit 81c150326c406f787930166cfbe39fd900e9031f.

--- ffmpegcommand2.sh
+++ ffmpegcommand2.sh
@@ -6,6 +6,6 @@
 " \
 [0:v]dummy_cuda[overlay_video];
 [1:v]scale_cuda=format=yuv420p[base];
-[base][overlay_video]overlay_cuda" \
+[base][overlay_video]overlay_cuda=x=640:y=0" \
 -an -c:v h264_nvenc overlay_test.mp4
 
